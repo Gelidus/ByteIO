348553146ff706ebf3cc378f0244e5bf089367b8
Gelidus <gelidus@hotmail.sk>
2015-04-16 10:25:54 +0200

    [Stream] Protected api implementation

--- ByteStream.cpp
+++ ByteStream.cpp
@@ -3,3 +3,32 @@
 //
 
 #include "ByteStream.h"
+
+using namespace std;
+
+ByteStream::ByteStream() { }
+
+ByteStream::ByteStream(Byte *input, unsigned int length) {
+	this->write(input, length);
+}
+
+ByteStream::~ByteStream() {
+
+}
+
+shared_ptr<Byte> ByteStream::read(unsigned int length) {
+	auto bytes = new Byte[length];
+
+	for (int i = 0; i < length; i++) {
+		bytes[i] = this->streamBuffer.front();
+		this->streamBuffer.pop_front();
+	}
+
+	return shared_ptr<Byte>(bytes);
+}
+
+void ByteStream::write(Byte *bytes, unsigned int length) {
+	for (int i = 0; i < length; i++) {
+		this->streamBuffer.push_back(bytes[i]);
+	}
+}
